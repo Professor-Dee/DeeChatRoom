11a9e8fc5038dad58c843426f9aefae51346046e
Professor-Dee <udeeokposin@gmail.com>
2021-05-10 23:18:26 +0100

    2nd commit

---
+++ app/src/main/java/com/example/deechatroom/DummyData.kt
@@ -0,0 +1,26 @@
+package com.example.deechatroom
+
+object DummyData {
+    val funFacts = listOf(
+            FactModel(
+                    name = "kotlin",
+                    R.drawable.kotlin_logo,
+                    fact = "The name kotlin was derived from an english island near Nigeria"
+            ),
+            FactModel(
+                    name = "java",
+                    R.drawable.java_logo,
+                    fact = "The name java was derived from an northern island near Nigeria"
+            ),
+            FactModel(
+                    name = "python",
+                    R.drawable.python_logo,
+                    fact = "The name kotlin was derived from an southern island near Nigeria"
+            ),
+            FactModel(
+                    name = "javascript",
+                    R.drawable.javascript_logo,
+                    fact = "The name kotlin was derived from an western island near Nigeria"
+            ),
+    )
+}
--- app/src/main/java/com/example/deechatroom/MainActivity.kt
+++ app/src/main/java/com/example/deechatroom/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.deechatroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.deechatroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding:ActivityMainBinding? = null
+    var adapter:FactAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setUpFact()
+    }
+
+
+    private fun setUpFact() {
+        adapter = FactAdapter( context = this, DummyData.funFacts)
+        binding?.factListView?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
