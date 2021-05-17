7e82c604b32dc82340a21f10ce97d61b424ecc92
Professor-Dee <udeeokposin@gmail.com>
2021-05-17 02:53:06 +0100

    3rd commit

--- app/src/main/java/com/example/deechatroom/FactAdapter.kt
+++ app/src/main/java/com/example/deechatroom/FactAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.deechatroom
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,12 +17,26 @@ class FactAdapter(context: Context,facts:List<FactModel>):ArrayAdapter<FactModel
              view = LayoutInflater.from(parent.context).inflate(R.layout.fact_item, parent, false)
         }
         val logo = view?.findViewById<ImageView>(R.id.imageLogo)
-        val name = view?.findViewById<TextView>(R.id.password_text_input)
+        val name = view?.findViewById<TextView>(R.id.nameTv)
         fact?.logo?.let {
             logo?.setImageResource(fact.logo)
         }
         name?.text = fact?.name
 
+
+         view?.setOnClickListener {
+             val intent = Intent(parent.context, DetailActivity::class.java)
+             intent.putExtra(LOGO_EXTRAS,fact?.logo)
+             intent.putExtra(NAME_EXTRAS,fact?.name)
+             intent.putExtra(FACT_EXTRAS,fact?.fact)
+             parent.context.startActivity(intent)
+         }
             return view!!
+    }
+
+    companion object{
+        val LOGO_EXTRAS = "logo_extras"
+        val NAME_EXTRAS = "name_extras"
+        val FACT_EXTRAS = "fact_extras"
     }
 }
--- app/src/main/java/com/example/deechatroom/LoginActivity.kt
+++ app/src/main/java/com/example/deechatroom/LoginActivity.kt
@@ -1,11 +1,67 @@
 package com.example.deechatroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        signIn()
+
+        setupSimpleSpinner()
+    }
+
+    private fun signIn() {
+        val username = findViewById<TextInputEditText>(R.id.username_edit_text)
+        val password = findViewById<TextInputEditText>(R.id.password_edit_text)
+        val loginButton = findViewById<MaterialButton>(R.id.login_button)
+
+        loginButton.setOnClickListener(){
+            if (username.toString().isNullOrEmpty() || password.toString().isNullOrEmpty()){
+                Toast.makeText(this,"Email or Password is not available", Toast.LENGTH_LONG).show()
+            }else {
+                if(username.toString() == "ProfessorDee" && password.toString() == "12345"){
+
+                    val intent = Intent(this, DetailActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this,"Wrong Email or Password", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun setupSimpleSpinner() {
+        val spinner = findViewById<Spinner>(R.id.simple_spinner)
+
+        val adapter = ArrayAdapter.createFromResource(this, R.array.language,android.R.layout.simple_spinner_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent!!.getItemAtPosition(position)
+                Toast.makeText(this@LoginActivity, "$selectedItem", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(this@LoginActivity, "No Item Was Selected", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
